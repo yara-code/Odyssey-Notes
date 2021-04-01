8e26a01bcaa2f24f324cfdcbfd2b0d8c41e8d3f0
gonzo-32 <gonzalez.heriberto97@gmail.com>
2021-03-31 20:15:01 -0700

    first

---
+++ components/Mainpage.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Mainpage() {
+  return (
+    <View>
+        <Text>fuck yara alot!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
