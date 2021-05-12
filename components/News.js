621e774a24944d873107d1563adbfff2600dde74
gonzo-32 <gonzalez.heriberto97@gmail.com>
2021-05-11 20:07:31 -0700

    yeee

--- components/News.js
+++ components/News.js
@@ -1,70 +1,98 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {View, StyleSheet, Text, ActivityIndicator, FlatList,Dimensions} from 'react-native'
 const {width,height} = Dimensions.get('window')
-export default class News extends React.Component{
-    
-    
-     state={
-            news:[],
-            loading: true
-        }
-    
 
-    fetchnews = () => {
-        fetch('GET https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY=3ddc86bc606a4606bea5004e0614a6cf')
-        .then((res)=>res.json())
-        .then((response)=>{
-            this.setState({
-                news: response.articles,
-                loading:false
-            })
-        })
-    }
-    
-    componentDidMount(){
-        this.fetchnews()
+
+export default function News() {
+    let [news, setNews] = useState([])
+    let [loading, setLoading] = useState('true')
+
+    const setLoadingHandler = (text) => {
+        setLoading(text)
     }
 
-    render(){
-        if(this.state.loading){
-            return (
-                <View Style= {{flex:1,alignItems:'center',justifyContent:'center',backgroundColor:'#333'}}>
-                    <ActivityIndicator size='large' colors='#fff'/>
-                </View> 
-            );
-        }else{
-            return(
-                    <View style={styles.container}>
-                    <View style={styles.header}>
-                        <Text style={{fonmtSize:25, color:'#fff'}}>Top Headline</Text>
-                        <Text style={{fontSize:35,color:'#fff'}}>Headline</Text>
+    const Newsfeed = () => {
+        <FlatList
+            data={news}
+            renderItem={({item})=>{
+                return(
+                    <View style ={{width:width-50,height:180,backgroundCOlor:'#fff',marginBottom:15}}>
 
                     </View>
-                    <View style={styles.news}>
-                        <FlatList
-                        data={this.state.news}
-                        renderItem={({item})=>{
-                                return(
-                                    <View style ={{width:width-50,height:180,backgroundCOlor:'#fff',marginBottom:15}}>
-
-                                    </View>
-                                )
-                            }}
-                        />
-                    </View>
-                </View>
-            )
-        }
+                )}}
+        />
     }
+
+    const getNews = async () => {
+        let res = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=f5037afaa4c647dfa836977a061e09e8')
+        const data = await res.json()
+                
+                console.log(data)
+
+                setNews.apply(data.articles)
+
+                console.log('News Array ' + news)
+
+                setLoadingHandler = 'false'
+    }
+
+
+    useEffect(
+        () => {
+            let interval;     
+
+            interval = setInterval(() => {
+                
+                newshandlers();
+            
+                    if(loading){
+                        return (
+                            <View Style= {{flex:1,alignItems:'center',justifyContent:'center',backgroundColor:'#333'}}>
+                                <ActivityIndicator size='large' colors='#fff'/>
+                            </View> 
+                        );
+                    }
+                    else{
+                        return(
+                                
+                                    <FlatList
+                                        data={news}
+                                        renderItem={({item})=>{
+                                        return(
+                                            <View style ={{width:width-50,height:180,backgroundCOlor:'#fff',marginBottom:15}}>
+
+                                            </View>
+                                        )}}
+                                    />
+                                         
+                        )
+                    }
+                
+            },10000)
+            
+            return () => clearInterval(interval);
+        });
+    
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={{fonmtSize:25, color:'#fff'}}>Top Headline</Text>
+                <Text style={{fontSize:35,color:'#fff'}}>Headline</Text>
+            
+            </View>
+            <View style={styles.news}> 
+                
+            </View>
+        </View>
+    )
+    
+        
 }
 
 const styles = StyleSheet.create({
 
-    container : {
+    container: {
         flex: 1,
         backgroundColor:'#333'
     }
-
-
-
 })
